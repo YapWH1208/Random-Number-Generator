9a49edd11876035b86dcfea53154f36276f47098
YapWH <107160166+YapWH1208@users.noreply.github.com>
2022-09-21 22:07:57 +0800

    2.0
    New version upload

--- Random Number Generator.py
+++ Random Number Generator.py
@@ -8,6 +8,7 @@ def refresh(V1, V2):
     Number.set(New_V)
 
 window = Tk()
+window.geometry("450x200")
 
 # Text Variable
 Min = StringVar()
@@ -15,30 +16,30 @@ Max = StringVar()
 Number = StringVar()
 
 # Label
-Title_Label = Label(window, text="Random Number Generator")
-Title_Label.grid(row=1, sticky=N)
+Title_Label = Label(window, text="Random Number Generator", font=("Arial", 24))
+Title_Label.grid(row=1, sticky=N, padx=20)
 
-Ins_Label = Label(window, text="Input the range of the number in the box")
+Ins_Label = Label(window, text="Input the range of the number in the box", font=("Arial", 16))
 Ins_Label.grid(row=3, sticky=N)
 
-Min_Label = Label(window, text="Minimum")
+Min_Label = Label(window, text="Minimum", font=("Arial", 12))
 Min_Label.grid(row=5, sticky=W)
 
-Max_Label = Label(window, text="Maximum")
+Max_Label = Label(window, text="Maximum", font=("Arial", 12))
 Max_Label.grid(row=6, sticky=W)
 
 Random_Label = Label(window, textvariable=Number, font=("Arial", 20))
 Random_Label.grid(row=8, sticky=S)
 
 # Entry
-Min_Entry = Entry(window, textvariable=Min)
+Min_Entry = Entry(window, textvariable=Min, font=("Arial", 12))
 Min_Entry.grid(row=5, sticky=E)
 
-Max_Entry = Entry(window, textvariable=Max)
+Max_Entry = Entry(window, textvariable=Max, font=("Arial", 12))
 Max_Entry.grid(row=6, sticky=E)
 
 # Button
-Refresh_Button = Button(window, text="Refresh", command=lambda: refresh(Min.get(),Max.get()))
+Refresh_Button = Button(window, text="Refresh", command=lambda: refresh(Min.get(), Max.get()), font=("Arial", 12))
 Refresh_Button.grid(row=7)
 
 window.mainloop()
